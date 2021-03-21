7881e5e3da8367df3ea8eef5a828e1cefd267f42
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2021-03-21 17:05:21 -0500

    Add Song Limit

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -3,14 +3,14 @@ import lyricsgenius
 import json
 import argparse
 from pathlib import Path
+import os
 
 # Constants
 DATA_DIR = Path('../data/')
 SECRETS = Path('./secrets.json')
 
-with open('secrets.json') as f:
-    secret_file = json.load(f)
-genius = lyricsgenius.Genius(secret_file["keys"]["genius"])
+# with open(SECRETS) as secrets_f:
+#     secrets = json.load(secrets_f)
 
 """
     This script downloads song data corresponding to the passed parameters
@@ -20,18 +20,19 @@ genius = lyricsgenius.Genius(secret_file["keys"]["genius"])
 """
 
 
-def download_genre(genre, page_limit=2):
+def download_genre(genius, genre, song_count_limit):
     """Download the data for a given genre, and save to the project's
     data directory
 
     Args:
         genre (str): genre to pull song data for
-        page_limit (int): limits number of pages to be scraped
+        song_count_limit (int): limits # of songs being pulled
+
     """
 
     page = 1
     all_lyrics = {}
-    while page < page_limit and page:
+    while len(all_lyrics) < song_count_limit and page:
         response = genius.tag(genre, page=page)
         for hit in response['hits']:
             lyrics = genius.lyrics(song_url=hit['url'])
@@ -43,35 +44,42 @@ def download_genre(genre, page_limit=2):
     return all_lyrics
 
 
-def download_artist(artist, page_limit=2, page_size=5):
+def download_artist(genius, artist, song_count_limit):
     """Download the data for a given artist, and save to the project's
     data directory
 
     Args:
+        genius (obj): genius API object
         artist (str): artist to pull song data for
-        page_limit (int): limits number of pages to be scraped
-        page_size (int): limits how many songs per page
+        song_count_limit (int): limits # of songs being pulled
     Returns:
         (str): name of artist
         (dict): dictionary containing song titles and lyrics
     """
 
-    genius_artist = genius.search_artist(artist, max_songs=1)
+    genius_artist = genius.search_artist(artist, max_songs=1, include_features=True)
     page = 1
-    songs = []
     all_lyrics = {}
+    temp = 0
 
-    while page < page_limit and page:
+    while len(all_lyrics) < song_count_limit and page:
         request = genius.artist_songs(genius_artist.id,
                                       sort='popularity',
-                                      per_page=page_size,
+                                      per_page=10,
                                       page=page)
-        songs.extend(request['songs'])
 
         for song in request['songs']:
+            print(temp)
+            temp += 1
             title = song['title']
-            lyrics = genius.search_song(title, genius_artist.name).lyrics
-            all_lyrics[title] = clean_lyrics(lyrics)
+            try:
+                lyrics = genius.search_song(title, genius_artist.name).lyrics
+                try:
+                    all_lyrics[title] = clean_lyrics(lyrics)
+                except IndexError:
+                    continue
+            except AttributeError:
+                continue
 
         page = request['next_page']
 
@@ -88,7 +96,7 @@ def clean_lyrics(lyrics):
     Returns:
         (str): clean lyrics
     """
-    lyrics.replace("\n", " ").replace("\r", " ")
+    lyrics = lyrics.replace("\n", " ").replace("\r", " ")
 
     lyrics_index = 0
     while lyrics_index < len(lyrics):
@@ -100,18 +108,33 @@ def clean_lyrics(lyrics):
 
         lyrics_index += 1
 
-    return lyrics
+    # return lyrics
+    # removes all non-unicode characters
+    return ''.join([i if ord(i) < 128 else ' ' for i in lyrics])
 
 
 def write_dict_to_file(artist_name, lyric_dict):
+    """ Writes passed dictionary to a file in the data folder, creates data
+    folder if it doesn't already exist
+
+    Args:
+        artist_name (str): name of the artist or genre
+        lyric_dict (dict): dictionary with song name as keys and lyrics as value
+    """
+
     formatted_artist_name = ""
-    for item in artist_name.split(" "):
-        formatted_artist_name += item + "_"
+    for name in artist_name.split(" "):
+        formatted_artist_name += name + "_"
 
-    file_path = formatted_artist_name + "lyrics.txt"
+    print(len(lyric_dict))
+    file_name = formatted_artist_name + "lyrics.txt"
+    file_path = str(Path().resolve().parent) + "/data/"
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
     data = str(lyric_dict)
-
-    file_writer = open(file_path, 'wt')
+    file_writer = open(file_path + file_name, 'wt')
     file_writer.write(data)
 
 
@@ -122,23 +145,23 @@ def main(args):
         args (object): command line arguments passed to the script
     """
 
-    # TODO : Make sure that one of genre or artist was passed as an argument
-
     # TODO : Make sure that the genre/artist passed is valid
-
-    # TODO : Launch download of data
 
     with open(SECRETS) as secrets_f:
         secrets = json.load(secrets_f)
+        genius = lyricsgenius.Genius(secrets['keys']['genius'])
         print('Key: {}\n'.format(secrets['keys']['genius']))
 
-    print('*****Passed Arguments*****\nGenre: {}\nArtist: {}'.format(args.genre, args.artist))
+    print('*****Passed Arguments*****\nGenre: {}\nArtist: '
+          '{}\nLimit: {}'.format(args.genre, args.artist, args.limit))
     print('This is the data directory: {}'.format(DATA_DIR))
 
+    song_count_limit = args.limit
+
     if args.artist is not None:
-        download_artist(args.artist)
+        download_artist(genius, args.artist, song_count_limit)
     if args.genre is not None:
-        download_genre(args.genre)
+        download_genre(genius, args.genre, song_count_limit)
 
 
 if __name__ == "__main__":
@@ -148,6 +171,8 @@ if __name__ == "__main__":
                         help='download music from the passed genre')
     parser.add_argument('--artist', dest='artist', type=str, default=None,
                         help='download music from the passed artist')
-                        
+    parser.add_argument('--limit', dest='limit', type=int, default=20,
+                        help='limits number of downloaded songs')
+
     args = parser.parse_args()
     main(args)
